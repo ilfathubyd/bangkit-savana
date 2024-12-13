ac74c041382f07eeef1d836801371011a1ecde6d
ilfathubyd <ilfathbadila@gmail.com>
2024-12-13 16:43:38 +0700

    first commit

---
+++ capstone-app/resources/views/index.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meubel House</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .hero-image {
+            background-image: url('https://lisha-gwegqtjubq.figweb.site/cdn-cgi/imagedelivery/s-dfVpmPR-aKHmwFNwAgnQ/lisha-gwegqtjubq.figweb.site-b8b32ba69b334d57fee4ebb9f34ad64155e259a4/w=1440');
+            height: 400px;
+            background-size: cover;
+            background-position: center;
+        }
+    </style>
+</head>
+<body>
+
+    <header class="bg-light py-3">
+        <div class="container">
+            <h1 class="text-center">Meubel House</h1>
+            <nav class="navbar navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#">Home</a>
+                <div class="collapse navbar-collapse">
+                    <ul class="navbar-nav ml-auto">
+                        <li class="nav-item"><a class="nav-link" href="#">Shop</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">About</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
+                    </ul>
+                </div>
+            </nav>
+        </div>
+    </header>
+
+    
+    <div class="hero-image"></div>
+
+    
+    <div class="container my-5">
+        <h2 class="text-center">Top Picks For You</h2>
+        <p class="text-center">Find your vacation spot and tourist destination that suits your taste with a variety of good and interesting places to choose from.</p>
+        
+        <div class="row">
+            <div class="col-md-4">
+                <img src="https://lisha-gwegqtjubq.figweb.site/cdn-cgi/imagedelivery/s-dfVpmPR-aKHmwFNwAgnQ/lisha-gwegqtjubq.figweb.site-16a0269db0e484548205499746dc7c8f1b4ee02a/w=328" class="img-fluid" alt="Garuda Wisnu Kencana">
+                <h5>Garuda Wisnu Kencana</h5>
+            </div>
+            <div class="col-md-4">
+                <img src="https://lisha-gwegqtjubq.figweb.site/cdn-cgi/imagedelivery/s-dfVpmPR-aKHmwFNwAgnQ/lisha-gwegqtjubq.figweb.site-f908158d7ed30e0bac3e9d310139f687cc8210cf/w=602" class="img-fluid" alt="Candi Prambanan">
+                <h5>Candi Prambanan</h5>
+            </div>
+            <div class="col-md-4">
+                <img src="https://lisha-gwegqtjubq.figweb.site/cdn-cgi/imagedelivery/s-dfVpmPR-aKHmwFNwAgnQ/lisha-gwegqtjubq.figweb.site-23218c1bf1fc5821b69b909a300ba79f297df1ab/w=602" class="img-fluid" alt="Pura Ulun Danu Beratan">
+                <h5>Pura Ulun Danu Beratan</h5>
+            </div>
+        </div>
+    </div>
+
+    
+    <footer class="bg-light py-4">
+        <div class="container text-center">
+            <h5>Our Instagram</h5>
+            <p>Follow our store on Instagram</p>
+            <p>400 University Drive Suite 200 Coral Gables, FL 33134 USA</p>
+            <p>Help | Payment Options | Returns | Privacy Policies</p>
+            <form class="form-inline justify-content-center">
+                <input type="email" class="form-control mb-2 mr-sm-2" placeholder="Enter Your Email Address">
+                <button type="submit" class="btn btn-primary mb-2">SUBSCRIBE</button>
+            </form>
+            <p>2022 Meubel House. All rights reserved.</p>
+        </div>
+    </footer>
+
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https:// stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
